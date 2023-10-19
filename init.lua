549579d9edc2de7b0b274c91fd6af52fa74b8f5c
frerebo <frerebo@nwmindustries.com>
2023-10-18 22:37:21 -0400

    updated directory structure for branch

--- init.lua
+++ init.lua
@@ -18,66 +18,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- NOTE: Here is where you install your plugins.
---  You can configure plugins using the `config` key.
---
---  You can also configure plugins after the setup call,
---    as they will be available in your neovim runtime.
+-- These are for stuff in `lua` directory
 require('lazy').setup("plugins")
-require('keybindings')
-
--- [[ Setting options ]]
--- See `:help vim.o`
--- NOTE: You can change these options as you wish!
-
--- Set highlight on search
-vim.o.hlsearch = false
-
--- Make line numbers default
-vim.wo.number = true
-
--- Enable mouse mode
-vim.o.mouse = 'a'
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = false
-
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
-
--- [[ Basic Keymaps ]]
+require('mappings')
+require('options')
 
 
-
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
---
+++ lua/mappings.lua
@@ -0,0 +1,63 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.keymap.set({'n', 'v'}, 'q', '<Nop>', {silent=true})
+vim.keymap.set('n', ';', ':')
+
+vim.keymap.set('n', '<M-k>', '<C-Y>', {silent=true})
+vim.keymap.set('n', '<M-j>', '<C-E>', {silent=true})
+
+-- vim.keymap.set('n', '<leader>e', ":NvimTreeToggle")
+
+vim.keymap.set('n', '<leader>e', ":NvimTreeToggle<CR>", {silent=true})
+
+vim.keymap.set('n', '<C-j>', "<C-w>j", {silent=true})
+vim.keymap.set('n', '<C-h>', "<C-w>h", {silent=true})
+vim.keymap.set('n', '<C-i>', "<C-w>i", {silent=true})
+vim.keymap.set('n', '<C-k>', "<C-w>k", {silent=true})
+
+
+
+
+
+
+
+
+--Mappings from original kicktstart init.nvim
+
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
