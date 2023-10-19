94ccf570f31c9fb031906f2d8ddbb9df130c418a
Frerebo <frerebo@nwmindustries.org>
2023-10-19 15:53:13 -0400

    moar mappings to resize

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -14,10 +14,10 @@ vim.keymap.set('n', '<M-j>', '2<C-E>', {silent=true})
 vim.keymap.set('n', '<leader>e', ":NvimTreeToggle<CR>", {silent=true})
 
 -- Window navigation
-vim.keymap.set('n', '<C-j>', "<C-w>j", {silent=true})
-vim.keymap.set('n', '<C-h>', "<C-w>h", {silent=true})
-vim.keymap.set('n', '<C-l>', "<C-w>l", {silent=true})
-vim.keymap.set('n', '<C-k>', "<C-w>k", {silent=true})
+vim.keymap.set({'n', 'c'}, '<C-j>', "<C-w>j", {silent=true})
+vim.keymap.set({'n', 'c'}, '<C-h>', "<C-w>h", {silent=true})
+vim.keymap.set({'n', 'c'}, '<C-l>', "<C-w>l", {silent=true})
+vim.keymap.set({'n', 'c'}, '<C-k>', "<C-w>k", {silent=true})
 
 vim.keymap.set({'n', 'v'}, "L", "$", {silent=true})
 vim.keymap.set({'n', 'v'}, "H", "0", {silent=true})
@@ -33,6 +33,11 @@ vim.keymap.set('n', 'tl', 'gt', {silent=true})
 vim.keymap.set('n', 'th', 'gT', {silent=true})
 vim.keymap.set('n', 'tx', '<cmd>tabclose<cr>', {silent=true})
 vim.keymap.set('n', '<leader>ta', '<cmd>tab ba<cr>', {silent=true}) -- open all buffers in tabs
+
+vim.keymap.set('n', '<M-[>', '<cmd>vertical resize +2<cr>', {silent=true})
+vim.keymap.set('n', '<M-]>', '<cmd>vertical resize -2<cr>', {silent=true})
+
+
 
 
 vim.api.nvim_set_keymap('n', 'J', ':normal o<Esc>', {noremap = true, silent = true}) --Insert blank line
