53866e5f26374936e902c0c6dba402dc6c68103d
Frerebo <frerebo@nwmindustries.org>
2023-10-19 11:02:05 -0400

    fix'd window navigation out of nvim-tree

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -15,7 +15,7 @@ vim.keymap.set('n', '<leader>e', ":NvimTreeToggle<CR>", {silent=true})
 
 vim.keymap.set('n', '<C-j>', "<C-w>j", {silent=true})
 vim.keymap.set('n', '<C-h>', "<C-w>h", {silent=true})
-vim.keymap.set('n', '<C-i>', "<C-w>i", {silent=true})
+vim.keymap.set('n', '<C-l>', "<C-w>l", {silent=true})
 vim.keymap.set('n', '<C-k>', "<C-w>k", {silent=true})
 
 
