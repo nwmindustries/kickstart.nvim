143d669ada53cc93b3451f76a068b6a39f706463
Frerebo <frerebo@nwmindustries.org>
2023-10-25 13:23:52 -0400

    fixed mapping problem for toggle term

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,10 +1,20 @@
 return {
 --   {'akinsho/toggleterm.nvim', version = "*", config = true}
+  -- 'akinsho/toggleterm.nvim',
+  -- version = "*",
+  -- config = function()
+  --   require('toggleterm').setup{
+  --     open_mapping = "\\"
+  --     
+  --     pj
+  --   }
+  -- end
   'akinsho/toggleterm.nvim',
-  version = "*",
-  config = function()
-    require('toggleterm').setup{
-      open_mapping = [[<leader>\]]
-    }
-  end
+  version = "",
+  opts = {
+    open_mapping = '\\',
+    insert_mappings = false
+  }
+
 }
+
