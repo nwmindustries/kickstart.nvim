dee1e0cd937047d5c71f76e8c565c9f5b5f4419c
Frerebo <frerebo@nwmindustries.org>
2023-11-01 15:59:22 -0400

    changed nvimtree on-attach to be inside plugin spec

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,29 +1,3 @@
--- nvim-tree-api.node.open.edit()
-
-local function my_on_attach(bufnr)
-  local api = require "nvim-tree.api"
-
-  local function opts(desc)
-    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
-  
-  local function open_and_refocus()
-    api.node.open.edit()
-    vim.cmd('NvimTreeFocus')
-  end
-
- -- custom mappings
-    -- default mappings
-  api.config.mappings.default_on_attach(bufnr) 
-  vim.keymap.set('n', 'o', open_and_refocus, opts('open and refocus')) 
-  vim.keymap.set('n', 'L', api.tree.change_root_to_node, opts("cd"))
-  vim.keymap.set('n', 'H', api.tree.change_root_to_parent, opts("cd .."))
-  vim.keymap.set('n', '.', api.tree.toggle_hidden_filter, opts("toggle dot"))
-   
-  -- vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
-  -- vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
-end
-
 return {
   "nvim-tree/nvim-tree.lua",
   version = "*",
@@ -33,7 +7,27 @@ return {
   },
   config = function()
     require("nvim-tree").setup {
-      on_attach = my_on_attach,
+      on_attach = function(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  local function open_and_refocus()
+    api.node.open.edit()
+    vim.cmd('NvimTreeFocus')
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+  vim.keymap.set('n', 'o', open_and_refocus, opts('open and refocus'))
+  vim.keymap.set('n', 'L', api.tree.change_root_to_node, opts("cd"))
+  vim.keymap.set('n', 'H', api.tree.change_root_to_parent, opts("cd .."))
+  vim.keymap.set('n', '.', api.tree.toggle_hidden_filter, opts("toggle dot"))
+
+end
+,
+
       update_focused_file = {
         enable= true,
       }
