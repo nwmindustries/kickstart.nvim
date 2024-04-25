ec7af635e0b3007f3addbabaf715560dabe2de6f
Frerebo <frerebo@nwmindustries.org>
2024-04-25 10:41:21 -0400

    saving before refactoring LSP st00f...

--- init.lua
+++ init.lua
@@ -1,28 +1,50 @@
--- TODO THIS IS NOW TO BE REFACTORED
 vim.g.loaded_netrw       = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+
+--@TODO HOLYSHITIMPORTANT TEMPORARIL REVERSING THE ORDER OF REQUIRING CORE AND LAYZ (in josean, he loads core, then lazy)
+require("frerebo.lazy")
+require("frerebo.core")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- TODO THIS IS NOW TO BE REFACTORED
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
-end
-vim.opt.rtp:prepend(lazypath)
 
--- These are for stuff in `lua` directory
-require('lazy').setup("plugins")
-require('autocmds')
-require('mappings')
-require('options')
+--@TODO
+
+-- local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+-- if not vim.loop.fs_stat(lazypath) then
+--   vim.fn.system {
+--     'git',
+--     'clone',
+--     '--filter=blob:none',
+--     'https://github.com/folke/lazy.nvim.git',
+--     '--branch=stable', -- latest stable release
+--     lazypath,
+--   }
+-- end
+-- vim.opt.rtp:prepend(lazypath)
+--
+-- -- These are for stuff in `lua` directory
+-- require('lazy').setup("plugins")
+-- require('autocmds')
+-- require('mappings')
+-- require('options')
 
 
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
---
+++ lua/frerebo/core/init.lua
@@ -0,0 +1,6 @@
+
+require("frerebo.core.options")
+require("frerebo.core.mappings")
+-- require("frerebo.core.nvim-ufo") --TODO commented out for refactor
+require("frerebo.core.autocmds")
+require("frerebo.core.tele")
